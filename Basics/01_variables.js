45891465cc43d5f8358a2a32727dc1339119f835
Thayyub126 <106765213+Thayyub126@users.noreply.github.com>
2024-07-30 18:10:13 +0000

    dataTypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = "23456";
+var accountEmail = "shaikthayyib@gamil";
+let accountpass = 123456;
+accoutCity = "delhi";
+
+let accountState; //getting undefined
+
+
+// accountId="123"; not allowed
+/*
+prefer not to use "var"
+because of issue in block scope and funcyional scope 
+ */
+accountEmail = "th@hgamilcom";
+accountpass = 6789
+accoutCity = "chennai";
+console.table([accountId,accountEmail,accountpass,accountState]);
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"  // treat all js code newer version
+
+let id = 2345;  // number
+let name= "thayyub"; //string
+let status = false;  //boolean
+let income ;        // undefined
+
+// null is a standalone value
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);    // object
+ 
