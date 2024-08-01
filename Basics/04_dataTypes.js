3605031501b4c749629564917138ac04b68d17a0
Thayyub126 <106765213+Thayyub126@users.noreply.github.com>
2024-08-01 17:41:17 +0000

    stack&heap

--- Basics/04_dataTypes.js
+++ Basics/04_dataTypes.js
@@ -8,3 +8,35 @@
 // NON- PRIMITIVE (REFERENCE TYPE)
 
 // 3 Types   ---> Array , Object , Functions
+
+
+/*============================= */
+
+// stack(primitive data types)                Heap(NON-primitive data types)
+
+
+// stack gives its copy value where as heap gives its reference value
+
+let oneNum = "Hai";
+let twoNum = oneNum;
+twoNum ="Hello";
+
+console.log(oneNum);
+console.log(twoNum);
+
+
+let user1 ={
+    name: "tahyyub",
+    no : 1234
+}
+
+let user2 = user1;
+
+user2.name= "webhash";
+
+
+console.log(user1.name);
+console.log(user2.name);
+
+
+
