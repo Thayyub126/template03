5fcb1fc927d90955c4a60f5b9fb955b511ea02d3
Thayyub126 <106765213+Thayyub126@users.noreply.github.com>
2024-08-01 17:25:00 +0000

    datatypes

---
+++ Basics/04_dataTypes.js
@@ -0,0 +1,8 @@
+// PRIMITIVE DATA TYPES
+
+// 7 TYPES  ---> Number , String , Boolean , Null , Undefined , Symbol , BigInt
+
+
+// NON- PRIMITIVE (REFERENCE TYPE)
+
+// 3 Types   ---> Array , Object , Functions
